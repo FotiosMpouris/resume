a87443a7a885b2dc83a2915d774ed64035f82da2
FotiosMpouris <126421111+FotiosMpouris@users.noreply.github.com>
2025-02-18 13:41:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -5,24 +5,23 @@ document.addEventListener("DOMContentLoaded", () => {
   const overlayBg = document.getElementById("overlay-bg");
   const overlayPopup = document.getElementById("overlay-popup");
 
-  // When hamburger is clicked
   hamburger.addEventListener("click", () => {
-    // Toggle hamburger "X" state
+    // Toggle the hamburger 'X' state
     hamburger.classList.toggle("active");
 
-    // Toggle the overlay and popup
+    // Toggle the overlay background and popup
     overlayBg.classList.toggle("open");
     overlayPopup.classList.toggle("open");
   });
 
-  // Close the overlay popup if user clicks on overlay-bg outside popup
+  // Close if user clicks on the background
   overlayBg.addEventListener("click", () => {
     hamburger.classList.remove("active");
     overlayBg.classList.remove("open");
     overlayPopup.classList.remove("open");
   });
 
-  // Close the popup when clicking a menu link
+  // Close if user clicks on a menu link
   const menuLinks = document.querySelectorAll(".overlay-menu a");
   menuLinks.forEach(link => {
     link.addEventListener("click", () => {
